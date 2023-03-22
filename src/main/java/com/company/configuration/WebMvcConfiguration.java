64ce0715338bdc6ebf7ca37ffd5f247735ccae5a
Vincent Zhang <vincentzhang@outlook.es>
2023-03-22 17:51:37 +0100

    fix error

--- src/main/java/com/company/configuration/WebMvcConfiguration.java
+++ src/main/java/com/company/configuration/WebMvcConfiguration.java
@@ -2,7 +2,6 @@ package com.company.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
