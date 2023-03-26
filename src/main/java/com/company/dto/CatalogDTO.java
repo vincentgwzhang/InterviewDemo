83db431d788f92aa6f54ce7a1c69b6531e3072d8
Vincent Zhang <vincentzhang@outlook.es>
2023-03-26 20:33:51 +0200

    add raw html template files

--- src/main/java/com/company/dto/CatalogDTO.java
+++ src/main/java/com/company/dto/CatalogDTO.java
@@ -1,4 +1,22 @@
 package com.company.dto;
 
+import lombok.Data;
+import org.apache.commons.collections4.map.HashedMap;
+
+import java.util.Map;
+
+@Data
 public class CatalogDTO {
+
+    private int id;
+
+    private String code;
+
+    private String imageUrl;
+
+    /**
+     * Map<catalog_code, Map<language_code, language>>
+     */
+    private Map<String, Map<String, String>> languages = new HashedMap<>();
+
 }
